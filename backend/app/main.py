a78fc0d7d03af4ca1ffc5b3cbe726c10e85d4e4e
Anto2003-cloud <antonio041103@gmail.com>
2026-04-26 01:17:50 -0400

    Configuración de CORS actualizada

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,10 @@ app = FastAPI(title="Jgystore API")
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://jgystore.vercel.app"  # Este es el importante
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
